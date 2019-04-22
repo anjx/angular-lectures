7d0f16b9b57d3007a46fedf1dbeb9bdf764ff178
Pavel Razuvalau <Pavel_Razuvalau@epam.com>
2019-04-22 17:10:55 +0300

    Add HTTP lecture

---
+++ angular-http/src/app/courses/pages/course-list-page/course-list-page.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../../../courses/models/course';
+import { CourseService } from '../../../courses/services/course.service';
+import { MatDialog } from '@angular/material';
+import { ConfirmationDialogComponent } from '../../components/confirmation-dialog/confirmation-dialog.component';
+import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime, filter } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-course-list',
+  templateUrl: './course-list-page.component.html',
+  styleUrls: ['./course-list-page.component.scss']
+})
+export class CourseListPageComponent implements OnInit {
+  courses: Course[] = [];
+  searchCriteria: string;
+
+  isLoading = false;
+  hasMoreCourses = true;
+  currentPage = 0;
+
+  private debounceSubject = new Subject<string>();
+
+  constructor(private courseService: CourseService,
+              private dialog: MatDialog,
+              private router: Router) {}
+
+  ngOnInit() {
+    this.loadMoreCourses();
+
+    this.debounceSubject
+      .pipe(
+        filter((value) => !value.length || value.length >= 3),
+        debounceTime(1000)
+      ).subscribe((searchCriteria) => {
+        this.reloadCourses(searchCriteria);
+      });
+  }
+
+  loadMoreCourses(searchCriteria?: string) {
+    if (!this.isLoading) {
+      this.isLoading = true;
+
+      this.courseService.getList(this.currentPage, searchCriteria).subscribe(({ courses, hasMoreCourses }) => {
+        this.courses = this.courses.concat(courses);
+
+        this.hasMoreCourses = hasMoreCourses;
+
+        this.isLoading = false;
+        this.currentPage++;
+      });
+    }
+  }
+
+  reloadCourses(searchCriteria?: string) {
+    this.currentPage = 0;
+    this.courses = [];
+    this.loadMoreCourses(searchCriteria);
+  }
+
+  editCourse(course: Course) {
+    this.router.navigate(['courses', course.id]);
+  }
+
+  removeCourse(course: Course) {
+    this.dialog.open(ConfirmationDialogComponent, {
+      width: '500px'
+    }).afterClosed().subscribe((isConfirmed) => {
+      if (isConfirmed) {
+        this.courseService.removeItem(course).subscribe(() => {
+          this.reloadCourses();
+        });
+      }
+    });
+  }
+
+  onSearchCriteriaChange(searchSriteria: string) {
+    this.debounceSubject.next(searchSriteria);
+  }
+}
---
+++ angular-http/src/app/courses/services/course.service.ts
@@ -0,0 +1,44 @@
+import { CourseResponse } from './../models/course';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, retry } from 'rxjs/operators';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+
+import { Course } from '../models/course';
+
+@Injectable()
+export class CourseService {
+  private readonly COURSES_URL = 'courses';
+  private readonly LIMIT = 10;
+
+  constructor(private http: HttpClient) {}
+
+  getList(page: number = 0, searchCriteria?: string): Observable<CourseResponse> {
+    const params = new HttpParams()
+      .set('start', (page * this.LIMIT).toString())
+      .set('count', (this.LIMIT).toString())
+      .set('textFragment', searchCriteria || '');
+
+    return this.http.get<Course[]>(this.COURSES_URL, { params })
+      .pipe(
+        retry(4),
+        map((courses) => ({ courses, hasMoreCourses: courses.length === this.LIMIT }))
+      );
+  }
+
+  createCourse(course: Course): Observable<Course>  {
+    return this.http.post<Course>(this.COURSES_URL, course);
+  }
+
+  getItemById(id: number): Observable<Course> {
+    return this.http.get<Course>(`${this.COURSES_URL}/${id}`);
+  }
+
+  updateItem(course: Course): Observable<Course> {
+    return this.http.put<Course>(`${this.COURSES_URL}/${course.id}`, course);
+  }
+
+  removeItem(course: Course): Observable<any> {
+    return this.http.delete(`${this.COURSES_URL}/${course.id}`);
+  }
+}
