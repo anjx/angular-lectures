df7521f4d035ddd3abba7a454edb4136aaf9f88e
Pavel Razuvalau <pavel@razuvalov.com>
2020-02-17 19:03:54 +0300

    Add base components to modules & services examples

--- angular-modules-services/src/app/app.module.ts
+++ angular-modules-services/src/app/app.module.ts
@@ -1,18 +1,32 @@
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PostsListComponent } from './components/posts-list/posts-list.component';
+import { PostComponent } from './components/post/post.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsListComponent,
+    PostComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
