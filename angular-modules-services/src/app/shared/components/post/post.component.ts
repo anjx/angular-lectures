41de4495b6754a5508027640036d0d884dacb152
Pavel Razuvalau <pavel@razuvalov.com>
2020-02-17 21:23:10 +0300

    Update Modules & Services examples

--- angular-modules-services/src/app/shared/components/post/post.component.ts
+++ angular-modules-services/src/app/shared/components/post/post.component.ts
@@ -1,15 +1,17 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { Post } from 'src/app/models/post';
+import { Post } from '../../models/post';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss']
+  styleUrls: ['./post.component.scss'],
 })
 export class PostComponent {
   @Input() post: Post;
   @Output() like = new EventEmitter<Post>();
   @Output() unlike = new EventEmitter<Post>();
+
+  constructor() {}
 
   likePost() {
     this.like.emit(this.post);
